505c0358a498f0f11584c5a26aa6f6afb4511d78
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2020-06-29 18:25:27 +0300

    init recipe

---
+++ RecipeJS/js/recipe.js
@@ -0,0 +1,43 @@
+
+
+function getData(){
+    let name = document.querySelector("#recipe-name").value;
+    let descr = document.querySelector("#description").value;
+    let types = document.querySelector("#types").value;
+    let diff =  document.querySelector('input[name="choice"]:checked').textContent;
+    let servs = document.querySelector("#servings").value;
+    let prepT = document.querySelector("#prepare").value;
+    let cookT = document.querySelector("#cooking").value;
+    
+    let recipe ={
+        Name: name,
+        Descr: descr,
+        Type: types,
+        Difficult: diff,
+        Servs: servs,
+        PrepT: prepT,
+        CookT: cookT
+    }
+
+    return recipe;
+}
+
+
+
+
+function formRecipe(){
+    let recipe = getData();
+    document.querySelector("form").classList.toggle(".display");
+    console.log(recipe);
+    document.querySelector("#r-name").innerText = recipe.Name;
+    document.querySelector("#r-type").innerText = recipe.Type;
+    document.querySelector("#r-people").innerText = recipe.Servs;
+    document.querySelector("#r-time").innerText = recipe.PrepT;
+    document.querySelector("#r-lvl").innerText = recipe.Difficult;
+    document.querySelector("#r-cook").innerText = recipe.CookT;
+    document.querySelector(".description").innerText = recipe.CookT;
+    document.querySelector(".ingredients").innerText = recipe.CookT;
+}
+
+let button = document.querySelector('input[type="button"]');
+button.addEventListener('click',formRecipe);
